ccc8ed0dce0411930bdbe66d16c3452f64ea7477
admin2 <admin2@HomePC>
2018-08-20 17:51:28 -0400

    Sample Hello World Script

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		//java.util.Date dt = new java.util.Date();
+		System.out.println("Hello World at: " + new java.util.Date().toString());		
+	}
+}
