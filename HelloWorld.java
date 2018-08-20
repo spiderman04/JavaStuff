2c7a84ff9d4cc8dd9540046c48552fde6b03fe1a
admin2 <admin2@HomePC>
2018-08-20 18:01:01 -0400

    Sample hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		//java.util.Date dt = new java.util.Date();
-		System.out.println("Hello World at: " + new java.util.Date().toString());		
+		System.out.println("Hello World at: " + new java.util.Date().toString());	
+		
+		
 	}
 }
